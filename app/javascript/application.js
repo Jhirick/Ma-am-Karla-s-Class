d143ded8cb282596d18b7b22da49f378bdf53c43
Renn <chrizraine.pena@gmail.com>
2025-11-01 23:31:30 +0800

    Edit Delete Button in Students

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const modal = document.getElementById("confirmDeleteModal");
+  const confirmBtn = document.getElementById("confirmDeleteBtn");
+
+  modal.addEventListener("show.bs.modal", event => {
+    const button = event.relatedTarget;
+    const url = button.getAttribute("data-student-url");
+    confirmBtn.setAttribute("href", url);
+    confirmBtn.setAttribute("data-turbo-method", "delete");
+  });
+});
+
+document.addEventListener("turbo:load", () => {
+  const modal = document.getElementById("confirmDeleteModal");
+  const confirmBtn = document.getElementById("confirmDeleteBtn");
+
+  if (!modal) return;
+
+  modal.addEventListener("show.bs.modal", event => {
+    const button = event.relatedTarget;
+    const url = button.getAttribute("data-student-url");
+    confirmBtn.setAttribute("href", url);
+    confirmBtn.setAttribute("data-turbo-method", "delete");
+  });
+});
+
