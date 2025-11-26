493a630a43b63c0aeb489bab723010e87c3e9151
Jhirick <floresjhirick@gmail.com>
2025-11-26 23:16:35 +0800

    [Task 23] Fixed new homepage

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Helloworldapplication
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << Rails.root.join("app", "assets", "videos")
+
   end
 end
