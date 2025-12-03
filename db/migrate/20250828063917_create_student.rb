85fcbc1377f5c868e2ed17fc4f0f945a4371be30
Kristel Anne S. Pabon <kristel0726@gmail.com>
2025-12-03 21:29:11 +0800

    About us page

---
+++ db/migrate/20250828063917_create_student.rb
@@ -0,0 +1,13 @@
+class CreateStudent < ActiveRecord::Migration[8.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :year_level
+      t.string :program
+      t.references :department, null: false,
+foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
